a034595ac1ab4052a145673b61ec45a7bc980bea
Your Name <your@email.com>
2026-05-28 00:22:10 +0545

    Fix frontend API fetch URL routing path

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('translate/', views.translate_view, name='translate'),
-    # path('api/translate/', views.TranslateWordAPI.as_view(), name='api_translate'),
+    path('api/translate/', views.TranslateWordAPI.as_view(), name='api_translate'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
 ]
