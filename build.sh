d7f7307d2ac51118776a263084a5bd95155a4124
Your Name <your@email.com>
2026-05-27 20:32:51 +0545

    Fix: Add automatic CSV import script

--- build.sh
+++ build.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+
+# Run migrations
 python manage.py migrate
+
+# Run the import script
+python import_data.py
+
+# Collect static files
+python manage.py collectstatic --no-input
